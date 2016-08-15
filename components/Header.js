9f47b9055e6e3ecce1c2e96a14bd525a46980509
Allan Liang <lianglv123456@gmail.com>
2016-08-15 21:32:19 +1200

    modify description

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ const Header = React.createClass({
         return (
             <div className="col-md-12 well cv-builder-header">
                     <div id="cv-builder-introduction" >
-                        This is a page built with React.js. You can enter your information on the right and it will show up on the left in real time. Have fun:)
+                        This is a page built with React.js and Bootstrap. You can enter your information on the right and it will show up on the left in real time. Have fun:)
                     </div>
             </div>
         );
--- components/Output/OutputForm/OutputInterest.js
+++ components/Output/OutputForm/OutputInterest.js
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 
 //Output-interest
