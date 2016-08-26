1807bf8afe589b8276eae9dd67ecbc5ffd7b9a9a
Allan Liang <lianglv123456@gmail.com>
2016-08-26 14:26:14 +1200

    refactor structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,14 @@ var webpack = require('webpack');
 module.exports = {
 	devtool: debug ? "inline-sourcemap" : null,
     entry: {
+    	// app: "./scripts"
     	app: "./scripts",
     	vendor: ["react","react-dom"]
     },
     output: {
         path: __dirname,
         filename: "bundle.min.js"
+        // filename: "scripts.min.js"
     },
     module: {
 	  loaders: [
@@ -22,15 +24,25 @@ module.exports = {
 	      }
 	    },
 	    {
+	        test: /\.jsx$/,
+	        loaders: ['babel-loader']
+	     },
+	    {
 	    	test: /\.css$/,
 	    	loader: "style!css"
 	    }
 	  ]
 	},
-	plugins: debug ? [] : [
+	devServer: {
+      inline: true,
+      port: 8080
+   },
+	plugins: debug ? [
+		new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
+	] : [
 		new webpack.optimize.DedupePlugin(),
 		new webpack.optimize.OccurenceOrderPlugin(),
-		new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-		new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
+		new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
+		new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
 	],
 };
